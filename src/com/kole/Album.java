4bccdacd7a611646a726321c3d8821de6236cc3b
Slobodan Kosanovic <slobodan.kosanovic@gmail.com>
2018-06-28 16:51:26 +0200

    a

--- src/com/kole/Album.java
+++ src/com/kole/Album.java
@@ -1,4 +1,64 @@
 package com.kole;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 public class Album {
+    private String name;
+    private String artist;
+    private ArrayList<Song> songs;
+
+    public Album(String name, String artist) {
+        this.name = name;
+        this.artist = artist;
+        this.songs = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public ArrayList<Song> getSongs() {
+        return songs;
+    }
+
+    public void addSongToPlayList(int songIndex, LinkedList<Song> playList){
+        songIndex-=1;
+        if (songIndex>=0 && songIndex<=this.songs.size()){
+            System.out.println("Added song " + this.songs.get(songIndex).getName() + " to the playlist");
+            playList.add(songIndex,this.songs.get(songIndex));
+        }else {
+            System.out.println("Song with index " + songIndex + " is not on this album");
+        }
+    }
+
+    public void addSongToPlayList(String songName, LinkedList<Song> playList){
+        if (findSong(songName)!=null){
+            System.out.println("Added " + songName + " to the playlist");
+            playList.add(findSong(songName));
+        }else {
+            System.out.println("Song " + songName + " does not exist on this album");
+        }
+    }
+
+    public void addSongToAlbum(String songName, double songDuration){
+        if (findSong(songName)==null){
+            System.out.println("Added song " + songName + " to this album");
+            this.songs.add(new Song(songName,songDuration));
+        }else {
+            System.out.println("Song " + songName + " already exists on this album");
+        }
+    }
+
+    private Song findSong (String songName){
+        for (Song tempSong : this.songs){
+            if (tempSong.getName().equals(songName)){
+                return tempSong;
+            }
+        }return null;
+    }
 }
--- src/com/kole/Main.java
+++ src/com/kole/Main.java
@@ -1,8 +1,48 @@
 package com.kole;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        ArrayList<Album> albums = new ArrayList<>();
+        LinkedList<Song> playList = new LinkedList<>();
+
+        Album album = new Album("Reload", "Metallica");
+
+        albums.add(album);
+
+        System.out.println("***************************************************");
+
+        album.addSongToAlbum("Fuel", 4.30);
+        album.addSongToAlbum("Slither", 5.13);
+        album.addSongToAlbum("Bad Seed", 4.05);
+        album.addSongToAlbum("Prince Charming", 6.05);
+
+        album = new Album("Sol Invictus", "Faith No More");
+
+        albums.add(album);
+
+        System.out.println("***************************************************");
+
+        album.addSongToAlbum("Superhero", 5.16);
+        album.addSongToAlbum("Motherfucker", 3.31);
+        album.addSongToAlbum("Matador", 6.09);
+        album.addSongToAlbum("Cone Of Shame", 4.40);
+
+        System.out.println("***************************************************");
+
+        albums.get(0).addSongToPlayList("Fuel", playList);
+        albums.get(0).addSongToPlayList("Slither", playList);
+        albums.get(0).addSongToPlayList("Bad Seed", playList);
+        albums.get(1).addSongToPlayList("Superhero", playList);
+        albums.get(1).addSongToPlayList("Matador", playList);
+        albums.get(1).addSongToPlayList("Cone Of Shame", playList);
+
+
+
+
+
     }
 }
