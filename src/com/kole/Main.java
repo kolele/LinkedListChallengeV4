f4f8742d2f5a3505fc593bb1d51d681c548526d2
Slobodan Kosanovic <slobodan.kosanovic@gmail.com>
2018-06-28 18:06:16 +0200

    a

--- src/com/kole/Main.java
+++ src/com/kole/Main.java
@@ -2,8 +2,12 @@ package com.kole;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.ListIterator;
+import java.util.Scanner;
 
 public class Main {
+
+    public static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
         ArrayList<Album> albums = new ArrayList<>();
@@ -40,9 +44,68 @@ public class Main {
         albums.get(1).addSongToPlayList("Matador", playList);
         albums.get(1).addSongToPlayList("Cone Of Shame", playList);
 
+        System.out.println("***************************************************");
 
+        play(playList);
 
+    }
 
+    public static void play(LinkedList<Song> playList){
+        ListIterator<Song> listIterator = playList.listIterator();
+        displayMenu();
+        boolean quit = false;
+        int choice = scanner.nextInt();
+        //scanner.nextLine();
+        while (!quit){
+            switch (choice){
+                case 0:
+                    displayMenu();
+                    break;
+                case 1:
+                    if (listIterator.hasNext()){
+                        System.out.println("Now playing " + listIterator.next().toString());
+                        break;
+                    }
+                case 2:
+                    System.out.println("Now playing " + listIterator.previous().toString());
+                    break;
+                case 3:
+                    if (listIterator.hasPrevious()){
+                        listIterator.previous();
+                        System.out.println("Replaying song " + listIterator.next().toString());
+                    }else {
+                        System.out.println("Reached end of the list");
+                    }
+                    break;
+                case 4:
+                    listSongs(playList);
+                    break;
+                case 5:
+                    listIterator.remove();
+                    break;
+                case 6:
+                    System.out.println("Quitting player");
+                    quit=true;
+                    break;
+            }
+        }
+    }
 
+    private static void displayMenu(){
+        System.out.println("Please enter a choice");
+        System.out.println("0 - to display this menu");
+        System.out.println("1 = to play next song in list");
+        System.out.println("2 - to play previous song in the list");
+        System.out.println("3 - to replay current song");
+        System.out.println("4 - to list songs in playlist");
+        System.out.println("5 - to remove current song");
+        System.out.println("6 - to quit this player");
+    }
+
+    private static void listSongs(LinkedList<Song> playList){
+        ListIterator<Song> listIterator = playList.listIterator();
+        while (listIterator.hasNext()){
+            System.out.println(listIterator.next().toString());
+        }
     }
 }
